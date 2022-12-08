1832b0a8a5efbe112378b553f44d4923b7ff2f7f
xing gao <2416457672@qq.com>
2022-12-08 12:35:10 +0800

    配置Mysql与注册登录模块(中)

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,11 +9,56 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+// import $ from 'jquery'
 
 export default {
   components: {
     NavBar
-  }
+  },
+  // setup() {
+  //   $.ajax({
+  //     url: "http://127.0.0.1:3000/user/account/token/",
+  //     type: "post",
+  //     data: {
+  //       username: "gx",
+  //       password: "aaa",
+  //     },
+  //     success(resp) {
+  //       console.log(resp);
+  //     },
+  //     error(resp) {
+  //       console.log(resp);
+  //     }
+  //   }),
+  //   $.ajax({
+  //     url: "http://127.0.0.1:3000/user/account/info/",
+  //     type: "get",
+  //     headers: {
+  //       Authoriztion: "Bearer " +"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzZDUyNThjNjQzOGU0ZWNiYjAxZGFkYTBhMzY2MjdjYiIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY3MDQ2ODAwMiwiZXhwIjoxNjcxNjc3NjAyfQ.l238oUss0npfU-CrSGlWYj25dEsZo9NA2GTmcTWuIGs"
+  //     },
+  //     success(resp) {
+  //       console.log(resp);
+  //     },
+  //     error(resp) {
+  //       console.log(resp);
+  //     }
+  //   }),
+  //   $.ajax({
+  //     url: "http://127.0.0.1:3000/user/account/register/",
+  //     type: "post",
+  //     data: {
+  //       username: "gx2",
+  //       password: "123",
+  //       confirmedPassword: "123",
+  //     },
+  //     success(resp) {
+  //       console.log(resp);
+  //     },
+  //     error(resp) {
+  //       console.log(resp);
+  //     }
+  //   })
+  // }
 }
 </script>
 
